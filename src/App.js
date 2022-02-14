ddd561491505ecbb26f4cde2f12dac000c8546d5
Daniel Kuc <danpkuc@gmail.com>
2022-02-14 20:43:42 +0000

    installed axios and added a test API call on page load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const makeGetRequest = async () => {
+    let response = await axios.get("https://theexchangeapi.azurewebsites.net/test");
+    console.log(response.status);
+    setMessage(response.data);
+  }
+
+  useEffect(() => {
+    makeGetRequest();
+  },[]);
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, World!</h1>
+        <h2>{message}</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
