3b8019e5e7da3999166308cb4e942e9e7b7348b2
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-02-17 08:13:42 +0000

    removed activeClassName - deprecated, and replaced with a function in className prop

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -5,27 +5,25 @@ const MainNav = () => (
   <div className="navbar-nav mr-auto">
     <NavLink
       to="/"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
+      exact="true"
+      className={({ isActive }) => "nav-link" + (isActive ? " router-link-exact-active" : "")}
     >
       Home
     </NavLink>
     <NavLink
       to="/profile"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
+      exact="true"
+      className={({ isActive }) => "nav-link" + (isActive ? " router-link-exact-active" : "")}
     >
       Profile
     </NavLink>
     <NavLink
-      to="/external-api"
-      exact
-      className="nav-link"
-      activeClassName="router-link-exact-active"
+      to="/admin"
+      exact="true"
+      className={({ isActive }) => "nav-link" + (isActive ? " router-link-exact-active" : "")}
+
     >
-      External API
+      Admin
     </NavLink>
   </div>
 );
