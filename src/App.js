61e90e325f5af9e667f85d5027828b16c65386ba
Daniel Kuc <danpkuc@gmail.com>
2022-03-09 20:59:13 +0000

    moved api call to Home component and setup layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Cart, Home, NavBar, Profile } from './components';
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './auth/protected-route';
 
-function App() {
-  const [message, setMessage] = useState("");
-
-  const makeGetRequest = async () => {
-    let response = await axios.get("https://theexchangeapi.azurewebsites.net/test");
-    console.log(response.status);
-    setMessage(response.data);
-  }
-
-  useEffect(() => {
-    makeGetRequest();
-  },[]);
-  
-  
+function App() {  
   return (
     <div>
       <NavBar/>
-      <header className="Main">
-        <h2>{message}</h2>
-      </header>
 
       <Routes>
         <Route path='/' element={<Home />} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Row } from 'react-bootstrap';
 
 const Home = () => {
+
+  const [message, setMessage] = useState("");
+
+  const makeGetRequest = async () => {
+    let response = await axios.get("https://theexchangeapi.azurewebsites.net/test");
+    console.log(response.status);
+    setMessage(response.data);
+  }
+
+  useEffect(() => {
+    makeGetRequest();
+  },[]);
+
   return (
-    <div>
-      <h2>Hello from Home</h2>
-    </div>
+    <Container>
+      <Row>
+        <h2>{`Hello from Home and ${message}`}</h2>
+      </Row>
+    </Container>
   )
 }
 
