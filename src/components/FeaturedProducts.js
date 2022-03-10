d7880bce3cfdfe5c1f5fd855f59819317f05df78
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-03-10 20:06:49 +0000

    created FeaturedProducts component

---
+++ src/components/FeaturedProducts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedProducts = () => {
+  return (
+    <div className='text-center'>
+      <p className='h2'>Featured Products</p>
+    </div>
+  )
+}
+
+export default FeaturedProducts
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import { Row } from 'react-bootstrap';
 import SearchBar from './SearchBar';
+import FeaturedProducts from './FeaturedProducts';
 
 const Home = () => {
-
-  const [message, setMessage] = useState("");
 
   const makeGetRequest = async () => {
     let response = await axios.get("https://theexchangeapi.azurewebsites.net/test");
     console.log(response.status);
-    setMessage(response.data);
   }
 
   useEffect(() => {
@@ -22,9 +19,7 @@ const Home = () => {
       <p className='h1 my-4'>The Exchange</p>
       <hr />
       <SearchBar/>
-      <Row>
-        <h2>{`Hello from Home and ${message}`}</h2>
-      </Row>
+      <FeaturedProducts/>
     </div>
   )
 }
