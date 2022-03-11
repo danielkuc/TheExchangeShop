51d44fbb8ae90c3701cb921af3667aca45596223
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-03-11 15:25:39 +0000

    started chnaging layout of Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,24 +1,18 @@
 import React from 'react';
-
 import { useAuth0 } from '@auth0/auth0-react';
+import { Container } from 'react-bootstrap';
 
 const Profile = () => {
   const { user } = useAuth0();
-  const { name, email } = user;
+  const { nickname, email } = user;
 
   return (
     <div>
-      <div className="row align-items-center profile-header">
-        <div className="col-md text-center text-md-left">
-          <h2>{name}</h2>
-          <p className="lead text-muted">{email}</p>
-        </div>
-      </div>
-      <div className="row">
-        <pre className="col-12 text-light bg-dark p-4">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
+      <Container className='text-center'>
+        <p>{nickname}</p>
+        <p>{email}</p>
+        {JSON.stringify(user, null, 2)}
+      </Container>
     </div>
   );
 };
