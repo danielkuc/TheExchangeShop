15a0ad168900f7921d5f3b6ded2e0b6bc1ae022a
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-08-20 08:00:32 +0100

    added props to Product model

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Button, Card, Col } from 'react-bootstrap'
 
-const Product = () => {
+const Product = ({productName, productDescription, price}) => {
   return (
     <Col lg="3" md="4" className='my-3'>
       <Card>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{productName}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            {productDescription}
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
