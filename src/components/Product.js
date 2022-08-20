59f386f3b1c4a483e949f4b2db45c58c0b05ca33
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-08-21 00:17:05 +0100

    added responsive layout of product model

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,16 +3,16 @@ import { Button, Card, Col } from 'react-bootstrap'
 
 const Product = ({productName, productDescription, price}) => {
   return (
-    <Col lg="3" md="4" className='my-3'>
-      <Card>
+    <Col lg="3" md="4" sm="6" xs="6"className='my-3'>
+      <Card className="productCard">
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
           <Card.Title>{productName}</Card.Title>
-          <Card.Text>{price}</Card.Text>
-          <Card.Text>
+          <Card.Text>£ {price}</Card.Text>
+          <Card.Footer className="text-center" >
             {productDescription}
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          </Card.Footer>
+            <Button variant="primary" className="w-100" >Go somewhere</Button>
         </Card.Body>
       </Card>
     </Col>
