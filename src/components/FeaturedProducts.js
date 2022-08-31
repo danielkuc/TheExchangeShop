4a337b8fcbf133a0e7b0f0a331d52226e7b1bb65
Daniel Kuc <danpkuc@gmail.com>
2022-08-31 21:56:13 +0100

    fixed a bug

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Row } from 'react-bootstrap'
-import Product from './Product'
+// import Product from './Product'
 
 const FeaturedProducts = () => {
   return (
     <>
       <p className='h2 pb-4'>Featured Products</p>
       <Row>
-        <Product/>
-        <Product/>
-        <Product/>
-        <Product/>
+        {/* <Product/> */}
+        {/* <Product/> */}
+        {/* <Product/> */}
+        {/* <Product/> */}
       </Row>
     </>
   )
