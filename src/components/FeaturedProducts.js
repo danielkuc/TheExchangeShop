fc434c865512374d9387b6fcafb0bb24c0e187e0
Daniel Kuc <danpkuc@gmail.com>
2022-09-01 17:49:16 +0100

    added featured products

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Row } from 'react-bootstrap'
-// import Product from './Product'
+import Product from './Product'
 
 const FeaturedProducts = () => {
+  const [featuredProjects, setFeaturedProjects] = useState({});
+
+  const getData = async () => {
+    const res = await fetch('data.json');
+    const data = await res.json();
+    return data;
+  }
+
+  useEffect( () => {
+    const fetchData = async () => {
+      const result = await getData();
+      console.log(result.results)
+      setFeaturedProjects(result.results);
+    }
+    fetchData();
+  } ,[]);
   return (
     <>
       <p className='h2 pb-4'>Featured Products</p>
       <Row>
-        {/* <Product/> */}
-        {/* <Product/> */}
-        {/* <Product/> */}
-        {/* <Product/> */}
+        {featuredProjects.map(product => <Product passedProduct={product} key={product.id} />)}
       </Row>
     </>
   )
